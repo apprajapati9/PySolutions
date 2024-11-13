827561b61c775866b67cda0d5856cb8d57d06596
Ajay P. Prajapati <apprajapati@hotmail.com>
2024-11-12 23:28:34 -0500

    init commit for python project- guess the number game impl in python

---
+++ GuessNumberGame.py
@@ -0,0 +1,27 @@
+import sys
+import random
+print("enter your name")
+name = input()
+
+try:
+    for i in name:
+        if i.isnumeric():
+            raise TypeError("Numbers not allowed in name")
+except TypeError as e:
+    print(e)
+    sys.exit()
+
+print("your name is " + name)
+answer = random.randint(1,20)
+guesses = 0
+guess = -1
+print("Guess number between 1 to 20")
+while guess != answer:
+    guess = int(input())
+    guesses+=1
+    if guess > answer:
+        print("try lower")
+    else:
+        print("try higher")
+
+print("you took {} guesses".format(guesses))
