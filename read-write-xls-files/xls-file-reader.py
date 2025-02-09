dd1b769a22de04404a56e60c07f4b682bb89a1ef
apprajapati <apprajapati@hotmail.com>
2025-02-08 23:44:02 -0500

    changes to html file reader

--- read-write-xls-files/xls-file-reader.py
+++ read-write-xls-files/xls-file-reader.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+from bs4 import BeautifulSoup
 
 #pd.set_option('display.max_columns', None)
 pd.set_option('display.encoding', 'utf-8')
@@ -11,25 +12,34 @@ entries = os.listdir('./files/')
 
 file_path = os.path.dirname(os.path.abspath(__file__))
 
-file_path += "\\files\\{}".format(entries[0])
+files = []
 
-print("File path: ", file_path)
+for i in entries:
+    path = file_path + "\\files\\{}".format(i)
+    files.append(path)
+    print(path)
 
-# xls_file = pd.ExcelFile(file_path, engine='openpyxl')
 
-data_file = pd.read_html(file_path, flavor='lxml', encoding ='utf-8')
+# xls_file = pd.ExcelFile(files[0], engine='openpyxl')
+
+data_file = pd.read_html(files[0], flavor='lxml', encoding ='utf-8', header=1, keep_default_na=True) #header=1 property one removes the 0th column that has no meaningful header for data.
 
 #pd.set_option('display.max_columns', None) 
 
-for i, table in enumerate(data_file):
-    print(f"Table {i}:")
-    print(table)
-    print("\n")
-
 output_file = "output.txt"
 
-data_file[0].map(lambda x: str(x).replace('\u25bc', ''))
+#data_file[0].map(lambda x: str(x).replace('\u25bc', ''))
 
-data_file[0].to_csv(output_file, sep = '\t', index= False, header = False, encoding='utf-8')
+# for i, table in enumerate(data_file):
+#     print(f"Table {i}:")
+#     print(table)
+#     print("\n")
 
-print(data_file[0].head())
+data_file[0].to_csv(output_file, sep = '\t', index= False, header = True, encoding='utf-8')
+
+#data = data_file[0].astype(str)
+
+#data.to_string(output_file, index= False, header = True, encoding='utf-8')
+
+
+#print(data_file[0].columns.tolist())
