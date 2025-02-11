262e0bc306cafe3e8ec9fac890733dcbace79aed
apprajapati <apprajapati@hotmail.com>
2025-02-10 22:25:04 -0500

    added logic to generate allteams and allplayers from xls team files from respective folders

--- read-write-xls-files/xls-file-reader.py
+++ read-write-xls-files/xls-file-reader.py
@@ -1,11 +1,10 @@
 import os
 import sys
 import pandas as pd
-from bs4 import BeautifulSoup
 from collections import defaultdict
 
 teams_map = {
-    "CLE": "Cleavland Cavaliers",
+    "CLE": "Cleaveland Cavaliers",
     "DET": "Detroit Pistons",
     "SAS": "San Antanio Spurs",
     "ATL": "Atlanta Hawks",
@@ -31,6 +30,8 @@ teams_map = {
     "DAL": "Dallas Mavericks",
     "IND": "Indiana Pacers",
     "LAL": "Los Angeles Lakers",
+    "BOS": "Boston Celtics",
+    "POR":"Portland Trail Blazers"
 }
 
 def replace_delim(output_file, delim = ""):
@@ -69,10 +70,12 @@ class Game:
         
 #pd.set_option('display.max_columns', None)
 pd.set_option('display.encoding', 'utf-8')
-print(sys.getdefaultencoding())
+#print(sys.getdefaultencoding())
 
 team_file_names = os.listdir('./files/teams')
 player_file_names = os.listdir('./files/players')
+all_players_file_names = os.listdir('./files/allplayers')
+all_teams_file_names = os.listdir('./files/allteams')
 
 # print(team_file_names)
 # print(player_file_names)
@@ -85,7 +88,8 @@ file_path = os.path.dirname(os.path.abspath(__file__))
 
 team_paths = []
 player_paths = []
-
+all_player_paths = []
+all_teams_paths = []
 #print("Teams file paths-->  \n")
 # for team files
 for i in team_file_names:
@@ -100,6 +104,14 @@ for i in player_file_names:
     player_paths.append(path)
     #print(path)
 
+for i in all_players_file_names:
+    path = file_path + "\\files\\allplayers\\{}".format(i)
+    all_player_paths.append(path)
+
+for i in all_teams_file_names:
+    path = file_path + "\\files\\allteams\\{}".format(i)
+    all_teams_paths.append(path)
+
 assert len(player_paths) == len(team_paths), "Don't have all data about the games" 
 
 # xls_file = pd.ExcelFile(files[0], engine='openpyxl')
@@ -110,16 +122,16 @@ for index, name in enumerate(team_file_names):
 for index, name in enumerate(player_file_names):
     player_file_names[index] = name[0:3]
 
-print("Currently Available data for teams and their team ID \n\n")
+print("\nCurrently Available team data and their ids from 1 to {}\n".format(len(team_paths)))
 available_teams_map = {}
 for index, name in enumerate(team_file_names):
     current = index + 1
     available_teams_map[current] = name
-    print(current, teams_map[available_teams_map[current]])
+    print("ID:",current, "| Team:", teams_map[available_teams_map[current]])
       
 assert len(team_file_names) == len(player_file_names), "Either Player or Team data is missing."
 
-print("Avail maps -> ", available_teams_map)
+#print("Avail maps -> ", available_teams_map)
 
 team_data_map = {}
 player_data_map = {}
@@ -142,25 +154,61 @@ for player_path in player_paths:
     player_data_map[team_id] = player_data
 
 
+all_players_output_file = "allplayers.txt"
+all_players_data = []
+
+for path in all_player_paths:
+    players_data = pd.read_html(path, flavor='lxml', encoding ='utf-8', header=1, keep_default_na=True, converters=defaultdict(lambda: str))    
+    all_players_data.append(players_data)
+
+with open(all_players_output_file, "w") as f:
+    pass
+
+with open(all_players_output_file, "w") as f:
+        f.write(f"\nData of All players \n\n")
+    
+for i in all_players_data:
+    i[0].to_csv(all_players_output_file, mode = 'a', sep = '\t', index= False, header = True, encoding='utf-8')
+
+all_teams_output_file = "allteams.txt"
+all_teams_data = []
+
+for index, path in enumerate(all_teams_paths):
+    if index == 0:
+        teams_data = pd.read_html(path, flavor='lxml', encoding ='utf-8', header=1, keep_default_na=True, converters=defaultdict(lambda: str))
+    else:
+        teams_data = pd.read_html(path, flavor='lxml', encoding ='utf-8', header=2, keep_default_na=True, converters=defaultdict(lambda: str))        
+    all_teams_data.append(teams_data)
+
+with open(all_teams_output_file, "w") as f:
+    pass
+
+with open(all_teams_output_file, "w") as f:
+        f.write(f"\nData of All teams games \n\n")
+    
+for i in all_teams_data:
+    i[0].to_csv(all_teams_output_file, mode = 'a', sep = '\t', index= False, header = True, encoding='utf-8')
+
+    
 game_size = len(team_file_names)//2
 
-assert game_size%2 != 0, "Data files are missing for certain teams. Please check."
+assert len(team_file_names)%2 == 0, "Data files are missing for certain teams. Please check."
 
 games = []
 
-
+i = 1
 while game_size != 0:   
-    team1_id, team2_id = input("Enter games team_ids using , separator").split(',')
+    team1_id, team2_id = input("\nEnter Game_{}'s team1 vs team2 ids using \",\" separator\n".format(i)).split(',')
 
     key1 = available_teams_map[int(team1_id)]
     key2 = available_teams_map[int(team2_id)]
-    print(team1_id, team2_id, key1, key2)
     games.append(Game(key1, key2))
     game_size -= 1
+    i += 1
 
 for index, game in enumerate(games):
-    print(game.team1_id, game.team2_id)
-    output_file = "Game {}.txt".format(index)
+    #print(game.team1_id, game.team2_id)
+    output_file = "Game {}.txt".format(index+1)
 
     with open(output_file, mode = 'w') :
         pass
@@ -190,13 +238,6 @@ for index, game in enumerate(games):
         f.write(f"\n\n\nTeam data of \"{teams_map[game.team2_id]}\"\n\n")
     team2[0].to_csv(output_file, mode = 'a', sep = '\t', index= False, header = True, encoding='utf-8')
 
-
-
-output_file = "output.txt"
-player_output_file = "players.txt"
-
-
-#data.to_string(output_file, index= False, header = True, encoding='utf-8') #This works but cannot have delimiter
-
     
 
+#data.to_string(output_file, index= False, header = True, encoding='utf-8') #This works but cannot have delimiter
